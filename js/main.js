631d31721b66cd87956b0daddeb5faa9ea8b0330
Lukasako <lukasmoralesarg@gmail.com>
2025-05-18 02:11:26 -0300

    primer commit - creacion nav e inicio

---
+++ js/main.js
@@ -0,0 +1,37 @@
+const typed = new Typed('.typed', {
+	strings: [
+		'<i class="mascota">Consultor Junior</i>',
+		'<i class="mascota">Estudiante Analista de Sistemas</i>',
+	],
+
+	//stringsElement: '#cadenas-texto', // ID del elemento que contiene cadenas de texto a mostrar.
+	typeSpeed: 75, // Velocidad en mlisegundos para poner una letra,
+	startDelay: 300, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
+	backSpeed: 75, // Velocidad en milisegundos para borrrar una letra,
+	smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
+	shuffle: false, // Alterar el orden en el que escribe las palabras.
+	backDelay: 1500, // Tiempo de espera despues de que termina de escribir una palabra.
+	loop: true, // Repetir el array de strings
+	loopCount: false, // Cantidad de veces a repetir el array.  false = infinite
+	showCursor: true, // Mostrar cursor palpitanto
+	cursorChar: '|', // Caracter para el cursor
+	contentType: 'html', // 'html' o 'null' para texto sin formato
+});
+
+//parallax 
+window.addEventListener('scroll', () => {
+	const seccion = document.querySelector('.seccion-inicio');
+	const fondo = document.querySelector('.fondo-parallax');
+  
+	if (!seccion || !fondo) return;
+  
+	const offset = window.scrollY;
+	const seccionTop = seccion.offsetTop;
+	const seccionHeight = seccion.offsetHeight;
+  
+	if (offset >= seccionTop - window.innerHeight && offset <= seccionTop + seccionHeight) {
+	  const velocidad = 0.5; // menor = más lento
+	  const desplazamiento = (offset - seccionTop) * velocidad;
+	  fondo.style.transform = `translateY(${desplazamiento}px)`;
+	}
+});
